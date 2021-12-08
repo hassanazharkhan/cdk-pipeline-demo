5fb12017f1c9ef08317e13b67653a9fec401dbe1
Hassan Azhar Khan <hassanazharkhan@gmail.com>
2021-12-08 11:22:36 +0500

    Add prod stage

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -1,5 +1,5 @@
 import { Construct, SecretValue, Stack, StackProps, Stage } from '@aws-cdk/core';
-import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines";
+import { CodePipeline, CodePipelineSource, ShellStep, ManualApprovalStep } from "@aws-cdk/pipelines";
 import { CdkpipelinesDemoStage } from './cdkpipelines-demo-stage';
 /**
  * The stack that defines the application pipeline
@@ -32,7 +32,15 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
       env: { account: '525813303752', region: 'us-east-1' }
      });
   
+     const prod = new CdkpipelinesDemoStage(this, 'prod', {
+      env: { account: '525813303752', region: 'us-east-1' }
+     });
     // This is where we add the application stages
     pipeline.addStage(preProd as any, { });
+    pipeline.addStage(prod as any, { 
+      pre: [
+        new ManualApprovalStep('PromoteToProd'),
+      ]
+    });
   }
 }
